91575e0862fbe04acfa35032711591937bb58727
Gowtham <jayavarapugowtham@gmail.com>
2026-07-24 00:21:03 +0530

    implemented the login with github enpoint

--- backend/api/v1/github.py
+++ backend/api/v1/github.py
@@ -1,6 +1,6 @@
 from fastapi.responses import RedirectResponse
 from fastapi import APIRouter
-
+from core.config import settings
 
 router = APIRouter(prefix="/api/v1/auth/github",tags=["Authentication"])
 
@@ -8,4 +8,11 @@ router = APIRouter(prefix="/api/v1/auth/github",tags=["Authentication"])
 
 @router.get("/login")
 def login_github():
-    return RedirectResponse("https://google.com")
+    github_auth_url = (
+        f"https://github.com/login/oauth/authorize"
+        f"?client_id={settings.GITHUB_CLIENT_ID}"
+        f"&redirect_uri={settings.GITHUB_REDIRECT_URI}"
+        f"&scope=read:user user:email"
+    )
+    return RedirectResponse(github_auth_url)
+
