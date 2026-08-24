abca3244dc117f32a3bf42d067a46f67e62aaac5
Gowtham <jayavarapugowtham@gmail.com>
2026-08-24 19:40:07 +0530

    Add post generation endpoint using WorkSummary and user preferences

--- backend/routes/posts.py
+++ backend/routes/posts.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,APIRouter,Depends,HTTPException,Query
-
-from model.summary.summary import WorkSummary
+import datetime
+from model.post.post import Post
 from sqlalchemy.orm import Session
 from time import time
 from services.llm_service import analyze_work
@@ -10,11 +10,31 @@ from db.session import get_db
 from core.dependency import get_current_user, create_access_token
 router = APIRouter(prefix="/post")
 
-@router.get("/generate_post")
-async def generate_post(
-     post_time: time,
-        timezone: str = Query("Asia/Kolkata"),
-        current_user=Depends(get_current_user),
-        db: Session = Depends(get_db)
-):  
-        db_llm_summary = db.query(WorkSummary).filter(WorkSummary.user_id==current_user.id).
+@router.post("/generate_post")
+async def generate_post_route(
+    work_summary_id: int,
+    platform: str,
+    post_length: int,
+    style: str,
+    inspiration: str,
+    excluded_topics: list[str],
+    current_user=Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    db_work_summary= db.query(WorkSummary).filter(WorkSummary.id==work_summary_id,WorkSummary.user_id==current_user.id).first()
+    if not db_work_summary:
+        raise HTTPException(status_code=404,detail="no work summary found")
+    post = await generate_post(db_work_summary,platform,post_length,style,inspiration,excluded_topics)
+    if post:
+        db_post = Post(
+                user_id=current_user.id,
+                work_summary_id=work_summary_id,
+                platform=platform,
+                content=post
+        )
+        db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return {
+        "post":post
+    }
