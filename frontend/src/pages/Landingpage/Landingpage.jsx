912729fc7ae1f88782e4b0ddeb3b5b2c8550cd27
Gowtham <jayavarapugowtham@gmail.com>
2026-08-26 14:27:39 +0530

    Add landing page and hero components

---
+++ frontend/src/pages/Landingpage/Landingpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./Hero/Hero";
+import "./LandingPage.css";
+const Landingpage = () => {
+  return (
+    <div className="landing-page">
+      <Hero />
+    </div>
+  );
+};
+
+export default Landingpage;
