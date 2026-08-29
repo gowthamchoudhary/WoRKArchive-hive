77980ac802ece338ff4bc84cc17e6706a61bf25e
Gowtham <jayavarapugowtham@gmail.com>
2026-08-30 00:40:53 +0530

    Update hero component

--- frontend/src/pages/Landingpage/Hero/Hero.jsx
+++ frontend/src/pages/Landingpage/Hero/Hero.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import black_logo from "../../../assets/black_logo_logs.png";
+import "./Hero.css";
 
 const Hero = () => {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <div className="main-text">
+        I build cool sh*t <br />❝ <span className="title-text">LOGS</span>
+        <span>
+          <img src={black_logo} className="logo-hero" />
+        </span>
+        remembers what I did .<br />
+        Then I post it ❞
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
