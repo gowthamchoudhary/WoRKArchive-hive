81227fc718ae55b0077108c30e127bc23cfa6745
Gowtham <jayavarapugowtham@gmail.com>
2026-09-02 23:52:53 +0530

    Add TextType component and update Hero with dependencies

--- frontend/src/pages/Landingpage/Hero/Hero.jsx
+++ frontend/src/pages/Landingpage/Hero/Hero.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import black_logo from "../../../assets/black_logo_logs.png";
+import TextType from "./TextType";
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="main-text">
-        I build cool sh*t <br />❝ <span className="title-text">LOGS</span>
+        <TextType
+          text={[
+            "I build cool sh*t",
+            "I design sleek interfaces",
+            "I ship products fast",
+          ]}
+          typingSpeed={60}
+          deletingSpeed={35}
+          pauseDuration={1800}
+          showCursor={true}
+          cursorCharacter="|"
+        />
+        <br />❝ <span className="title-text">LOGS</span>
         <span>
-          <img src={black_logo} className="logo-hero" />
+          <img src={black_logo} className="logo-hero" alt="Logs logo" />
         </span>
         remembers what I did .<br />
         Then I post it ❞
