a6a301b6c269a2d4389399230ccd0e5e3cd7ce9e
Gowtham <jayavarapugowtham@gmail.com>
2026-09-03 22:44:55 +0530

    Update application routes and route protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import LandingPage from "./pages/Landingpage/Landingpage";
-
+import Dashboard from "./pages/Dashboard/Dashboard";
 import "./App.css";
 import Auth from "./pages/Auth/Auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
