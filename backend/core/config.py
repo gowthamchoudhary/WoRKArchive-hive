25f5f8ce1f9eafafb4b7947a6f1902f70f622d51
Gowtham <jayavarapugowtham@gmail.com>
2026-09-05 17:08:54 +0530

    Update application settings and configuration

--- backend/core/config.py
+++ backend/core/config.py
@@ -13,6 +13,8 @@ class Settings(BaseSettings):
     REFRESH_TOKEN_EXPIRE_DAYS:int
     GROQ_API_KEY:str  
     GROQ_MODEL:str
+    FRONTEND_URL: str = "http://localhost:5173"
+    CORS_ORIGINS: str = "http://localhost:5173"
     model_config = SettingsConfigDict(
         env_file=".env"
     )
